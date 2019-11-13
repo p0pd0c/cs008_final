044f72fb716fc84334f8270b8c7aed141d3b1ecf
Jared DiScipio <jared.michael.discipio@gmail.com>
2019-11-12 22:39:28 -0500

    added page to view all menu items

--- list-items.php
+++ list-items.php
@@ -3,7 +3,21 @@
         ?>
 
         <main>
-            
+            <?php
+                include 'sql.php';
+                
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $sql = "SELECT * FROM tblMenuItems";
+                
+                $statement = $pdo->query($sql);
+                
+                print '<ol>';
+                while($row = $statement->fetch()) {
+                    print '<li>$row[fldname]: $row[flddescription], \$$row[fldprice]</li>';
+                }
+                print '</ol>';
+            ?>
         </main>
 
         <?php
