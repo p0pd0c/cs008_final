414e60d3973d5cac334a6ca6858b368f7949bafb
Jared DiScipio <jared.michael.discipio@gmail.com>
2019-11-15 17:58:06 -0500

    made it a postback, might change later... also need to use fieldsets

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php 
     include 'top.php';
 ?>
-    <form class="form-signin" method="POST" action="handle_login.inc.php">
+    <form class="form-signin" method="POST" action=<?php print $phpSelf; ?>>
         <section class="text-center mb-4">
             <!-- REPLACE W/ Bytebakery logo: img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" -->
             <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
