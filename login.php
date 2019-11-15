73d1f885398de6970937299009513cc1c3ed234c
Jared DiScipio <jared.michael.discipio@gmail.com>
2019-11-15 17:52:37 -0500

    Removed divs from login.php

--- login.php
+++ login.php
@@ -2,27 +2,27 @@
     include 'top.php';
 ?>
     <form class="form-signin" method="POST" action="handle_login.inc.php">
-        <div class="text-center mb-4">
-            <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-            <h1 class="h3 mb-3 font-weight-normal">Floating labels</h1>
-            <p>Build form controls with floating labels via the <code>:placeholder-shown</code> pseudo-element. <a href="https://caniuse.com/#feat=css-placeholder-shown">Works in latest Chrome, Safari, and Firefox.</a></p>
-        </div>
+        <section class="text-center mb-4">
+            <!-- REPLACE W/ Bytebakery logo: img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" -->
+            <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
+            <p>You must sign in in order to access this part of the site</p>
+        </section>
 
-        <div class="form-label-group">
+        <section class="form-label-group">
             <input id="txtEmail" class="form-control" placeholder="Email address" required="" autofocus="">
             <label for="txtEmail">Email address</label>
-        </div>
+        </section>
 
-        <div class="form-label-group">
+        <section class="form-label-group">
             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
             <label for="inputPassword">Password</label>
-        </div>
+        </section>
 
-        <div class="checkbox mb-3">
+        <section class="checkbox mb-3">
             <label>
             <input type="checkbox" value="remember-me"> Remember me
             </label>
-        </div>
+        </section>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     </form>
 </main>
