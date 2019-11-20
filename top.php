d1bf15df0f35fbe84ad8df4c0d7b2851d70529cf
Jared DiScipio <jareddiscipio@ip0af53dd2.int.uvm.edu>
2019-11-20 10:01:51 -0500

    Added session_start() here so it runs on all pages

--- top.php
+++ top.php
@@ -11,8 +11,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <?php
-        
+            // Starting session on every single page for authentication... session will store auth token
+            session_start();
+
+            // Filename of the currently executing script
             $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+
+            // Path parts are the little breadcrumbs that let me know what page I am on by looking at the script name
             $path_parts = pathinfo($phpSelf);
         
             print '<title>';
@@ -32,7 +37,9 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="./css/custom.css">
 
+        
         <?php 
+            // Include bootstrap experimental stylesheet on login and signup pages
             if ($path_parts['filename'] == 'login' || $path_parts['filename'] == 'signup') {
                 print '<link href="https://getbootstrap.com/docs/4.3/examples/floating-labels/floating-labels.css" rel="stylesheet">';
             }
