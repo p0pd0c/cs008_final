fee8f2016cd591447572390a1017794a46eeb422
Jared DiScipio <jareddiscipio@Jareds-MBP.fios-router.home>
2019-11-30 13:04:51 -0500

    modified

--- store.php
+++ store.php
@@ -1,11 +1,16 @@
-<php 
+<?php 
     include 'auth_config.php';
-    include 'sql.php';
+
+    // Check if user is logged in... if not then kick em out
+    $uid = access_control();
+    
+    // Top half of my page with meta tags and title, etc... main is opened beforehand
     include 'top.php';
 ?>
 
+    
 </main>
 
-<?php 
+<?php
     include 'footer.php';
 ?>
